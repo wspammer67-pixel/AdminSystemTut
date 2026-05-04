f03baf844917f3e8b02f8fd5013a0fe4c85d3807
wspammer67-pixel <wspammer67@gmail.com>
2026-05-03 21:22:38 -0500

    Add AdminUsers.lua for admin user configuration
    Created AdminUsers.lua to define admin user details.

---
+++ AdminUsers.lua
@@ -0,0 +1,9 @@
+local AdminUser = {
+	User = 2036382883, -- Replace with your userid
+	Username = "djmallett_0407" -- Replce with your user
+	
+}
+
+return AdminUser
+
+-- Must be Module Script, Put in ReplicatedStorage
